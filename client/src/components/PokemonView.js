af34bb00f503f905487112c9e3e0b6dbc4ca7f35
Nivm200 <Nivm200@gmail.com>
2021-03-20 01:27:17 +0200

    Worked on pokemon list
    with samuel and amit

--- client/src/components/PokemonView.js
+++ client/src/components/PokemonView.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
+import TypeList from "./TypeList";
 export default class PokemonView extends Component {
+  getAllTypes(type) {
+    return <TypeList type={type} />;
+  }
   render() {
+    const newTypes = this.props.data.types?.map((type) => {
+      return (
+        <span
+          onClick={() => {
+            console.log(type.name);
+            this.getAllTypes(type.name);
+          }}
+        >
+          {type.name + ` `}{" "}
+        </span>
+      );
+    });
+
     return (
       <div className="pokemon-view">
         <div className="details">
+          <TypeList type={"fire"} />
           <ul className="details-list">
             <li>{`id: ${this.props.data.id}`}</li>
             <li>{`Height: ${this.props.data.height}`}</li>
             <li>{`Weight: ${this.props.data.weight}`}</li>
             <li>{`name: ${this.props.data.name}`}</li>
-            <li>{`types: ${this.props.data.types
-              ?.map((type) => type.name)
-              .join()}`}</li>
+            <li>{/* <span>{`types: `}</span>
+              {newTypes} */}</li>
           </ul>
           <img src={this.props.data.sprites?.front} />
         </div>
