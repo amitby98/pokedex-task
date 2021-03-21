20724a29906af094c7378868ae0fb2cb24f9e0dd
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 04:44:06 +0200

    updated app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
 app.use("/api", api);
 
 module.exports = app;
