5d57af7a54a3c06fae5e897d7d706e2492ecc888
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 05:48:29 +0200

    heroku 2

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const api = require("./routes");
+const api = require("./server/routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("../client/build"));
+app.use(express.static("./client/build"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "../client/build/index.html");
+  res.sendFile(__dirname + "./index.html");
 });
 
 app.use("/api", api);
