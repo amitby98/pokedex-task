4b13f5400cdef3d85cd997adea937038e5a1018e
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 23:58:18 +0200

    updated build and files

--- client/src/components/PokemonView.js
+++ client/src/components/PokemonView.js
@@ -129,11 +129,11 @@ export default class PokemonView extends Component {
             updatePokemon={this.props.updatePokemon}
           />
         ) : null}
-        {/* <div class="center-on-page">
+        <div class="center-on-page">
           <div class="pokeball">
             <div class="pokeball__button"></div>
           </div>
-        </div> */}
+        </div>
       </div>
     );
   }
