4b2ce16d0b2905b7525f3991e334f2296e336230
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 23:46:24 +0200

    updated files

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const path = require("path");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname + "/build")));
+app.use(express.static(path.join(__dirname + "/client", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build", "index.html"));
+  res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
 });
 
 app.use("/api", api);
