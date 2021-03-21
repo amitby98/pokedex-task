29882f4533156086237c6c421137fcb0ee1b7775
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 04:41:35 +0200

    updated app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.use("/api", api);
 
