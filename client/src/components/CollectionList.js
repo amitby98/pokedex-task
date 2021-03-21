540df61791baa1fb5961b397557e5a075d48aa35
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-21 23:41:35 +0200

    changed and updated build

--- client/src/components/CollectionList.js
+++ client/src/components/CollectionList.js
@@ -5,37 +5,16 @@ class CollectionList extends Component {
   render() {
     return (
       <div>
-        <h1>collection</h1>
+        {/* <h1>collection</h1> */}
         <ul>
           {this.props.list.map((pokemon) => {
             return (
-              // <li>
-              //   <div>{pokemon.data.name}</div>
-              //   <div>
-              //     <img src={pokemon.data.sprites.front}></img>
-              //   </div>
-              // </li>
-              // <div className="center">
-              //   <div className="pokedex_top">
-              //     <div
-              //       className="pokedex_button"
-              //       onClick={() => {
-              //         this.clickme();
-              //       }}
-              //     >
-              //       <div className="pokedex_button_button" />
-              //     </div>
-              //   </div>
-              //   <div className="back_pokedex">
               <li>
                 <div>{pokemon.data.name}</div>
                 <div>
                   <img src={pokemon.data.sprites.front} />
                 </div>
               </li>
-              //   </div>
-              //   <div className="pokedex_bottom" />
-              // </div>
             );
           })}
         </ul>
